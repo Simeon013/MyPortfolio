3612beda403526715341f418255120ca99ad5c64
Siméon <simeondaouda@gmail.com>
2023-04-30 01:07:08 +0100

    link for create page

--- resources/js/Pages/Projects/Create.vue
+++ resources/js/Pages/Projects/Create.vue
@@ -4,17 +4,17 @@ import { Head, Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Projects Index" />
+    <Head title="Projects Create"/>
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Projects</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">New Project</h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-end m-2 p-2">
-                    <Link :href="route('projects.create')" class="px-4 py-2 bg-indigo-700 text-white rounded-md">New Project</Link>
+                        Form
                 </div>
             </div>
         </div>
